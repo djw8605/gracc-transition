4db4078a2c8b6fb9e9f88215f817ca09b883315f
Derek Weitzel <djw8605@gmail.com>
2020-03-06 17:09:28 -0600

    Fixing the probe

--- probe.py
+++ probe.py
@@ -1,20 +1,22 @@
+from __future__ import print_function
+
 import requests
 from elasticsearch import Elasticsearch
 
 
 
-UPLOADDEST = "https://..."
+UPLOADDEST = "https://gracc-transition.herokuapp.com/upload/gracc.opensciencegrid.org/"
 
 def main():
-    client = Elasticsearch()
+    client = Elasticsearch(timeout=60)
     r=client.indices.stats(metric='docs,store')
     token = ""
     with open("token.txt", 'r') as token_file:
         token = token_file.read()
     
-    headers = {"Authorization": "Bearer {}".format(token)}
+    headers = {"Authorization": "Bearer {}".format(token.strip())}
     
-    print(requests.post(UPLOADDEST, r['indices']), headers = headers)
+    print(requests.post(UPLOADDEST, data=r['indices'], headers=headers))
 
 
 if __name__ == "__main__":
